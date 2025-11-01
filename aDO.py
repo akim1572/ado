f40ef7c895cb13a70fb05ce82532314efa320008
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-11-01 11:20:35 -0400

    init

---
+++ aDO.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+def main():
+    current_files = os.listdir()
+
+    print("akim's Directory Organizer")
+    print(current_files)
+
+if __name__ == "__main__":
+    main()
