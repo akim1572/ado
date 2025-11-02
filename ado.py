8f24d4a33fc9e0485c6afa4b198e1219c9c00635
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-11-02 15:58:05 -0500

    reading in file extensions, now i have to figure out how to run through whatever and what not to do what is what is necessary for what is to become

--- ado.py
+++ ado.py
@@ -4,21 +4,21 @@ import os
 def importExtensions():
     with open('file_extensions.json', 'r') as ext_file:
         data = json.load(ext_file)
-        print(data)
+    
+    return data
 
 def main():
     current_files = os.listdir()
 
     print("akim's Directory Organizer")
 
+    extension_data = importExtensions()
+
     for i in range(len(current_files)):
         root, extension = os.path.splitext(current_files[i])
 
         if not extension:
             continue
-
-        importExtensions()
-
 
 
 if __name__ == "__main__":
