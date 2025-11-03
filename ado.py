b952cb4d8756d15c83df61534a950c0acb8ec7fa
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-11-02 22:49:24 -0500

    it is now working

--- ado.py
+++ ado.py
@@ -1,11 +1,22 @@
 import json
 import os
+import shutil
 
 def importExtensions():
     with open('file_extensions.json', 'r') as ext_file:
         data = json.load(ext_file)
     
     return data
+
+def testDirectory(name):
+    if not os.path.isdir(name):
+        os.mkdir(name)
+
+def testExtension(extension, data):
+    for i in range(len(data)):
+        if extension in data[i]["extensions"]:
+            testDirectory(data[i]["name"])
+            return data[i]["name"]
 
 def main():
     current_files = os.listdir()
@@ -19,6 +30,9 @@ def main():
 
         if not extension:
             continue
+
+        new_dir = testExtension(extension, extension_data)
+        shutil.move(current_files[i], new_dir)
 
 
 if __name__ == "__main__":
